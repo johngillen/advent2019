f1c4fc121e0abb6a57fbc1f7743c62b5385bc2a2
John Gillen <john.gillen@trojans.dsu.edu>
2022-12-07 22:48:45 -0600

    lost 5 days of code :(

---
+++ day01.py
@@ -0,0 +1,11 @@
+f = open('input/day01.txt')
+lines = [line.rstrip() for line in f.readlines()]
+
+for line in lines:
+    print(line)
+
+part1 = 0
+part2 = 0
+
+print(f'part 1: {part1}')
+print(f'part 2: {part2}')
