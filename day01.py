0f049f1abfc32265dff77b582032db83e9530f22
John Gillen <john.gillen@trojans.dsu.edu>
2022-12-15 01:31:40 -0600

    day 1

--- day01.py
+++ day01.py
@@ -1,11 +1,13 @@
 f = open('input/day01.txt')
 lines = [line.rstrip() for line in f.readlines()]
 
-for line in lines:
-    print(line)
+def fuel(mass):
+    x = mass // 3 - 2
+    if x <= 0: return 0
+    return fuel(x) + x
 
-part1 = 0
-part2 = 0
+part1 = sum([int(line) // 3 - 2 for line in lines])
+part2 = sum([fuel(int(line)) for line in lines])
 
 print(f'part 1: {part1}')
 print(f'part 2: {part2}')
